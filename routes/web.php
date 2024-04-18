3eb29076f920445794e9fcb7001c418fc947fe3f
zisan2108 <zisan979@gmail.com>
2024-04-18 12:56:57 +0600

    Customize Login Form(14)

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,16 @@ Route::middleware(['auth','role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashbord'])->name('admin.dashbord');
 
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
-});
+
+}); // End Group Admin Middleware
+
+
 
 Route::middleware(['auth','role:agent'])->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashbord'])->name('agent.dashbord');
-});
+
+});  // End Group Admin Middleware
+
+
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
